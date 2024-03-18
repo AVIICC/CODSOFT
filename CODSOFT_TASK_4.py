2b65fe8feee30c61278e1d7866ca910026a8823d
Alok Chauhan <alokchavhan44@gmail.com>
2024-03-18 10:41:27 +0530

    task 4 optional task

--- CODSOFT_TASK_4.py
+++ CODSOFT_TASK_4.py
@@ -55,13 +55,18 @@ scissors = '''
 
 images = [rock,paper,scissors] 
 
+rounds = 0
+player_score = 0
+computer_score = 0
+
 want_to_exit = False
 while not want_to_exit:
-        
 
+    rounds+=1
+    print(f"Round {rounds} over")
     choice = int(input('''\n------------------\n       Menu\n------------------
 0. Rock\n1. Paper\n2. Scissors\nChoose any one option from above by there number: '''))
-
+    print(f"\n-------\nRound - {rounds}\n-------\n")
     if choice>=3:
         print("You enterd invalid operation")
     else:
@@ -69,23 +74,41 @@ while not want_to_exit:
         com_choice = r.randint(0,2)
         print("\nComputer selected",images[com_choice])
         if choice==2 and com_choice==0:
+            computer_score = 100
             print("You loose")
+
         elif com_choice==2 and choice==0:
+            player_score = 100
             print("You Win")
+
         elif choice>com_choice:
+            player_score = 100
             print("You Win !")
+
         elif com_choice>choice:
+            computer_score = 100
             print("You loose")
+
         elif choice==com_choice:
             print("its draw")
-        another_try = input("\nIf want to play again press \"yes\" or press  \"no\" : ").lower()
-        if another_try == "yes":
-            continue
-        elif another_try == "no":
-            want_to_exit = True
-            print("Thanks, have a nice day.")
-        else:
-            print("Invalid input try again.")
-            
 
+        print(f'''--------------------------------
+Scores\nplayer score = {player_score}    computer score = {computer_score}
+ --------------------------------\n ''')
 
+        if rounds == 3:
+            another_try = input("\nIf you want to play again press \"yes\" or press \"no\" to exit: ").lower()
+            if another_try == "yes":
+                continue
+            elif another_try == "no":
+                want_to_exit = True
+                print("Thanks, have a nice day.")
+            else:
+                print("Invalid input, try again.")
+
+if player_score > computer_score:
+    print("You won the game!")
+elif player_score < computer_score:
+    print("Computer won the game!")
+else:
+    print("It's a tie game!")            
